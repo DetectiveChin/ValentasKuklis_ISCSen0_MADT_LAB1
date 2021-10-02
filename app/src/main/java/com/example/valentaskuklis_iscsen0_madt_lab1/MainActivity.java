3d59d25c424ab10dbb8e198765aad527cc6f674f
DetectiveChin <91797123+DetectiveChin@users.noreply.github.com>
2021-10-02 22:21:41 +0300

    Added:	A Button and TextView elements to Activity Layout file;         Updated Activity code file with the code that handles Button click event and changes TextView content with some text;

--- app/src/main/java/com/example/valentaskuklis_iscsen0_madt_lab1/MainActivity.java
+++ app/src/main/java/com/example/valentaskuklis_iscsen0_madt_lab1/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.valentaskuklis_iscsen0_madt_lab1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView TvMain;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.TvMain = findViewById(R.id.TvMain);
+    }
+
+    public void onBtnClick(View view) {
+        this.TvMain.setText("Text was changed!!!");
     }
 }
