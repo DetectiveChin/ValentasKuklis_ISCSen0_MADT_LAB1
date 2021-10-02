6fe7aa7427b547f4c68561de1ff118565e8e0cd5
DetectiveChin <91797123+DetectiveChin@users.noreply.github.com>
2021-10-02 23:10:55 +0300

    Added: A second Button element and make the code changes to change the text color;

--- app/src/main/java/com/example/valentaskuklis_iscsen0_madt_lab1/MainActivity.java
+++ app/src/main/java/com/example/valentaskuklis_iscsen0_madt_lab1/MainActivity.java
@@ -2,11 +2,16 @@ package com.example.valentaskuklis_iscsen0_madt_lab1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tv;
+    Button btn;
 
     private TextView TvMain;
     @Override
@@ -14,6 +19,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         this.TvMain = findViewById(R.id.TvMain);
+        // I'm initializing button and text view here
+        tv = (TextView) findViewById(R.id.textView2);
+        btn = (Button) findViewById(R.id.buttonColor);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btn.setTextSize(20);
+                tv.setText("Text color has changed");
+                tv.setTextSize(18);
+
+                tv.setTextColor(Color.parseColor("#FF0000"));
+            }
+        });
     }
 
     public void onBtnClick(View view) {
